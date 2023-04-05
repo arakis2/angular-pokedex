08a0d38fbb0843cc8029adc0e4a2c2a619e829e1
arakis2 <51448379+arakis2@users.noreply.github.com>
2023-04-05 19:43:43 +0200

    mise en place de l'async

--- src/app/pokemon/list-pokemon/list-pokemon.component.ts
+++ src/app/pokemon/list-pokemon/list-pokemon.component.ts
@@ -13,7 +13,8 @@ export class ListPokemonComponent implements OnInit {
   constructor(private router: Router, private pokemonService: PokemonService){}
 
   ngOnInit(): void {
-    this.pokemonList = this.pokemonService.getPokemonList();
+    this.pokemonService.getPokemonList().subscribe((pokemonList) => 
+    this.pokemonList = pokemonList);
   }
   goToPokemon(pokemon: Pokemon){
     this.router.navigate(['/pokemon', pokemon.id])
--- src/app/pokemon/pokemon.service.ts
+++ src/app/pokemon/pokemon.service.ts
@@ -1,21 +1,50 @@
 import { Injectable } from '@angular/core';
 import { Pokemon } from './pokemon';
-import { POKEMONS } from './mock-pokemon-list';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable, catchError, of, tap } from 'rxjs';
 
 @Injectable()
 export class PokemonService {
+  private pokemons: Pokemon[];
 
-  constructor() { }
+  constructor(private  httpClient: HttpClient) { }
 
-  getPokemonList():Pokemon[]{
-    return POKEMONS;
+  getPokemonList(): Observable<Pokemon[]>{
+    return this.httpClient.get<Pokemon[]>('api/pokemons').pipe(
+      tap((pokemonList: any) => this.log(pokemonList)),
+      catchError((error) => this.handleError(error, []))
+    )
   }
 
-  getPokemonById(pokemonId: number):Pokemon|undefined{
-    return POKEMONS.find(p => p.id == pokemonId);
+  getPokemonById(pokemonId: number): Observable<Pokemon|undefined>{
+    return this.httpClient.get(`api/pokemons/${pokemonId}`).pipe(
+      tap((pokemon: any) => this.log(pokemon)),
+      catchError((error) => this.handleError(error, []))
+    );
   }
 
-  getPokemonTypeList(): string[]{
-    return [...new Set(POKEMONS.flatMap(p => p.types))];
+  private log(response: any){
+    console.table(response);
+  }
+
+  private handleError(error: Error, errorValue: []|any){
+    console.error(error);
+    return of(errorValue);
+  }
+
+  updatePokemon(pokemon: Pokemon): Pokemon|undefined{
+    const httpOptions = {
+      headers: new HttpHeaders({'Content-Type': 'application/json'})
+    };
+
+    this.httpClient.put('api/pokemons', pokemon, httpOptions).pipe(
+      tap((response) => {
+        this.log(response);
+        return pokemon;
+      }),
+      catchError((error) => this.handleError(error, undefined))
+    );
+
+    return undefined;
   }
 }
