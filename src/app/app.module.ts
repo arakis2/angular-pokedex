8d8bd9a37607522d2bb7059de9d7eeb2a3f2b6bf
arakis2 <51448379+arakis2@users.noreply.github.com>
2023-04-06 16:23:57 +0200

    dernière version + MEP

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,14 @@ import { PageNotFoundComponent } from './page-not-found/page-not-found.component
 import { PokemonModule } from './pokemon/pokemon.module';
 import { FormsModule } from '@angular/forms';
 import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
-import {InMemoryDataService} from './in-memory-data.service'
+import {InMemoryDataService} from './in-memory-data.service';
+import { LoginComponent } from './login/login.component'
 
 @NgModule({
   declarations: [
     AppComponent,
-    PageNotFoundComponent
+    PageNotFoundComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
